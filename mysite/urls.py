6e87636fa90ed583af6e19ea4a4ac02dec78bf22
natureworknews <hadezahadeza@gmail.com>
2020-04-09 23:35:15 +0100

    My Nework app, first commit

---
+++ mysite/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.conf import settings
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+]
+
+if settings.DEBUG: # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+
+
+
+    
+
